9e678d62e97eb8c59169ed2f088cf60e78cd4b63
Cassiano Medeiros <cassiano.medeiros.93@gmail.com>
2024-10-10 15:36:25 -0400

    fetches data from 2 different APIs implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
  <>
+ <Typography variant="h4">First API Call</Typography>
  <FetchData/>
+ <Divider sx={{ margin: '20px 0' }} />
+ <Typography variant="h4">Second API Call</Typography>
+ <FetchYogaData/>
  </>
   )
 }
